499dc91633904314f6be3801b3b0b1d2277999e7
Gago <egagik69@gmail.com>
2022-03-30 17:26:59 +0400

    change mySplace

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ Array.prototype.myForEach = function (back) {
 
 arr.myForEach(
 	(item) => {
-		console.log(item)
+	console.log(item)
 	});
 
 
@@ -18,52 +18,44 @@ arr.myForEach(
 
 
 function mySplice(arr1, start, end, ...args) {
+	debugger
 	let size = 0;
 
+	let qount = start + end;
+	for (let i = 0; i < arr1.length; i++) {
 
-	function createStart() {
-		let starts = [];
+		if (i > qount) {
+			arr1[i - end] = arr1[i];
+		}
+	}
 
-			for (let i = 0; i < start; i++) {
-				size += 1;
-				starts.push(arr1[i]);
+	while (size < end) {
+		size += 1;
+		arr1.pop();
+	}
+
+	if (args.length) {
+		for (let j = 0; j < args.length; j++) {
+			arr1.push(args[j]);
+		}
+
+		let changeIndex = arr1.length - args.length;
+		let max = changeIndex + args.length;
+		let tmp = '';
+		for (let g = start; g < arr1.length; g++) {
+			if (g > start && g < changeIndex && changeIndex < max) {
+				tmp = arr1[g];
+				arr1[g] = arr1[changeIndex];
+				arr1[changeIndex] = tmp;;
+				changeIndex += 1;
 			}
-		
-		return starts;
+		}
 	}
 
-	startArry = createStart();
-
-	function createEnd() {
-		let ends = [];
-
-		end += size;
-		for (let i = end; i < arr1.length; i++) {
-			ends.push(arr1[i]);
-		}
-
-		if (args.length) {
-			for (let a = 0; a < args.length; a++) {
-				ends.push(args[a])
-			}
-		}
-		return ends
-	}
-
-	let endArray = createEnd();
-
-
-	function nowArr() {
-		for (let i = 0; i < endArray.length; i++) {
-			startArry.push(endArray[i])
-		}
-
-		return startArry;
-	}
-	arr1 = nowArr();
 	return arr1;
+
 }
 
-
 let arr1 = [34, 5, 65, 4, 44, 23];
-console.log('my ', mySplice(arr1, 2, 1));
+console.log(mySplice(arr1, 2, 1));
+
