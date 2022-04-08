04d6b0f8c31b2a76f04b1b74eb9ac11b4860454c
Gago <egagik69@gmail.com>
2022-04-08 23:49:53 +0400

    create table,get posts

--- index.js
+++ index.js
@@ -1,61 +1,94 @@
-// 1. foreach 
-let arr = [55, 6, 58, 89];
-
-Array.prototype.myForEach = function (back) {
-	for (i = 0; i < this.length; i++) {
-		back(this[i], i, this);
-	}
-}
-
-arr.myForEach(
-	(item) => {
-		console.log(item)
-	});
 
 
-// 2. splice
+class Table {
+	url = "https://jsonplaceholder.typicode.com/";
+	usersPost = [];
+	constructor() { }
 
-
-
-function mySplice(arr1, start, end, ...args) {
-	debugger
-	let size = 0;
-
-	let qount = start + end;
-	for (let i = 0; i < arr1.length; i++) {
-
-		if (i > qount) {
-			arr1[i - end] = arr1[i];
-		}
+	getPost() {
+		fetch(`${this.url}posts`)
+			.then((response) => {
+				return response.json()
+			}).then((data) => {
+				this.usersPost = data;
+				const table = document.createElement('table');
+				this.createTable()
+			})
 	}
 
-	while (size < end) {
-		size += 1;
-		arr1.pop();
+
+
+	createTable() {
+
+		const table = document.createElement('table');
+		const thead = this.createThead();
+		const tbody = this.createTbody();
+		const body = document.body;
+		table.appendChild(thead);
+		table.appendChild(tbody);
+		table.classList.add('table')
+		body.appendChild(table);
+		console.log(table)
 	}
 
-	if (args.length) {
-		for (let j = 0; j < args.length; j++) {
-			arr1.push(args[j]);
-		}
-
-		let changeIndex = arr1.length - args.length;
-		let max = changeIndex + args.length;
-		let tmp = '';
-		for (let g = start; g < arr1.length; g++) {
-			if (g > start && g < changeIndex && changeIndex < max) {
-				tmp = arr1[g];
-				arr1[g] = arr1[changeIndex];
-				arr1[changeIndex] = tmp;;
-				changeIndex += 1;
+	createThead() {
+		const tr = document.createElement('tr');
+		if (this.usersPost.length) {
+			const keys = Object.keys(this.usersPost[0]);
+			keys.push('Action');
+			for (let i = 0; i < keys.length; i++) {
+				const th = document.createElement('th');
+				th.textContent = keys[i];
+				th.classList.add('th')
+				tr.appendChild(th);
 			}
 		}
+
+
+		const thead = document.createElement('thead');
+		thead.append(tr);
+		return thead
 	}
 
-	return arr1;
+	createTbody() {
+		const tbody = document.createElement('tbody');
+		if (this.usersPost.length) {
+			for (let i = 0; i < this.usersPost.length; i++) {
+				const tr = document.createElement('tr');
+				const values = Object.values(this.usersPost[i]);
+				const arrSrc = ['imgs/box.png', 'imgs/edit.png', 'imgs/eye.png'];
+				values.push(arrSrc);
 
+				for (let j = 0; j < values.length; j++) {
+					const td = document.createElement('td');
+					td.classList.add('td');
+					if (Array.isArray(values[j])) {
+						for (let k = 0; k < values[j].length; k++) {
+							const img = new Image(17, 17);
+							img.src = values[j][k];
+							td.appendChild(img);
+							tr.append(td);
+
+						}
+
+						
+					} else {
+						td.textContent = values[j];
+						tr.append(td);
+					}
+				}
+				tbody.appendChild(tr)
+			}
+		}
+
+		return tbody;
+	}
+
+	
 }
 
-let arr1 = [34, 5, 65, 4, 44, 23];
-console.log(mySplice(arr1, 2, 1));
 
+
+const table = new Table();
+
+table.getPost()
